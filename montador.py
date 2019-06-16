f4ed93965c683c174188cad186184f64c0a4ca6d
Marcos de Souza Boger <marcos.boger@visagio.com>
2019-06-16 19:56:39 -0300

    Passo 1 completo

--- montador.py
+++ montador.py
@@ -1,3 +1,4 @@
+import re
 #Arquivo base do montador de 2 passos 
 
 #Tabela de simbolos (dicionario)
@@ -29,9 +30,10 @@ tab_mnemonicos = {'J':{'tamanho':2},
                   'IO':{'tamanho':1}}
 tab_mnemonicos_pseudo = {'@', '#', '$','K'}
 #variaveis
-passo = 1
-CI = 0
+passo = 1 #passo atual do montador 
+CI = 0 
 acumulador = 0
+erro = False #indica se houve erro no passo
 
 if(passo == 1):
     with open('./codigo_fonte/teste.txt', 'r') as arquivo:
@@ -42,6 +44,7 @@ if(passo == 1):
             if(len(palavras) == 3): 
                 if(palavras[0] in tab_simbolos and tab_simbolos[palavras[0]]['status'] == 'definido'):
                     print("Erro: Multipla definicao de simbolo ("+palavras[0]+")")
+                    erro = True
                     break
                 elif(palavras[0] in tab_simbolos and tab_simbolos[palavras[0]]['status'] == 'indefinido'):
                     tab_simbolos[palavras[0]]['status'] = 'definido'
@@ -52,9 +55,33 @@ if(passo == 1):
             #Analise do Mnemonico
             if(not(palavras[0] in tab_mnemonicos or palavras[0] in tab_mnemonicos_pseudo)):
                 print("Erro: Mnemonico invalido ("+palavras[0]+")")
+                erro = True
                 break
+            #mnemonico esta definido, tratar operador
+            #separo os mnemonicos pelos possiveis tipo de operador (rotulo ou enderecamento direto)
+            if(1):
+                if((palavras[1] in tab_simbolos and tab_simbolos[palavras[1]]['status'] == 'indefinido') or not(palavras[1] in tab_simbolos)):
+                    tab_simbolos[palavras[1]] = {'linha':'-', 'status':'indefinido'}
+                    isEnderecoConvertido = False
+                if(palavras[1] in tab_simbolos and tab_simbolos[palavras[1]]['status'] == 'definido'):
+                    endereco_convertido = tab_simbolos[palavras[1]]['linha']
+                    isEnderecoConvertido = True
+            elif(palavras[1][0] != '/'):
+                print("Erro: Endereco deve ser hexadecimal com valor de 0000 a FFFF comecando com /")
+                erro = True
+                break
+            else:
+                endereco_convertido = palavras[1][1]+palavras[1][2]+palavras[1][3]+palavras[1][4]
+            
             CI = CI + tab_mnemonicos[palavras[0]]['tamanho']
-   
             linha = arquivo.readline() #le nova linha
-print(CI)
-print(tab_simbolos)        
+    for simbolo in tab_simbolos:
+        if(tab_simbolos[simbolo]['status'] == 'indefinido'):
+            print("Erro: simbolo não definido (" + simbolo + ")")
+            erro = True
+    if(erro == True):
+        print("Erro ocorrido! Montagem não foi feita com sucesso")
+    else: 
+        print(CI)
+        print(tab_simbolos)   
+        passo == 2     
